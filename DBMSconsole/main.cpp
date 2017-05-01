1804de6360243e9aa5e1fda1c360091cd900822f
wsxst <280373133@qq.com>
2017-05-01 15:56:32 +0800

    框架V1.1
    增加了对表一级的操作

--- DBMSconsole/main.cpp
+++ DBMSconsole/main.cpp
@@ -1,3 +1,4 @@
+//包含文件
 #include <QCoreApplication>
 #include <iostream>
 #include <string>
@@ -7,13 +8,50 @@
 #include <QTextStream>
 #include <QDir>
 #include <vector>
+#include <set>
+//宏定义
 #define UNLEN 5//用户名最大长度
 #define PWDLEN 10//密码最大长度
 #define CT //创建表的宏
 #define DT //删除表
-#define RT //重命名表
+//命名空间
 using namespace  std;
+//结构体
+typedef struct
+{
+    int year;
+    int month;
+    int day;
+    int hour;
+    int minute;
+    int second;
+}Date;
+typedef struct
+{
+    string name;//字段名
+    string type;//类型
+    int length;//长度
+    int constraint[7];
+    /*七种约束条件，值为0代表无，其余代表有
+     * 0代表主键，1代表外键，2代表unique，
+     * 3代表check（1代表检查值是否存在于set中，2代表检查值是否在最大和最小值之间，3代表是否在两日期范围中），
+     * 4代表default（1代表整型，2代表字符串，3代表日期），
+     * 5代表非空，
+     * 6代表自增*/
+
+    set<string> scope;//取值的集合
+    Date mindate;//最早日期
+    Date maxdate;//最晚日期
+    int minvalue;//最小值
+    int maxvalue;//最大值
+
+    int defaultint;//默认整型
+    string defaultchar;//默认字符串
+    Date defaultdate;//默认日期
+}Field;
+//全局变量
 int a[] = {4, 5, 3, 2, 4, 5, 1};//加密用整型数组
+//功能函数
 string EnterPassword();//输入密码，返回值为输入的密码
 bool JudgeUser(string username,string password);//判断用户名和密码是否存在、是否匹配
 void encryption(string& c, int a[]);//对写入文件的密码进行加密操作
@@ -25,6 +63,15 @@ int renameDBEntity(string newName,string oldName);//重命名数据库实体
 int dropDBEntity(string DBname);//删除数据库实体
 int useDBEntity(string DBname);//打开并使用数据库
 int initDBEntity(string DBname);//初始化数据库
+int backupDBEntity(string DBname);//备份数据库
+int createTable(string TBname,string DBname,vector<Field> fields);//创建表
+int dropTable(string TBname,string DBname);//删除表
+int deleteField(string TBname,string DBname,string colname);//删除表中一列
+int addField(string TBname,string DBname,Field col);//增加一列
+int modifyCol(string TBname,string DBname,string oldname,string newname,string type,int length);//修改一列的名称及类型
+int renameTable(string TBname,string DBname,string newname);//重命名表
+int removePri(string TBname,string DBname);//删除主键
+int addPri(string TBname,string DBname,string priname,vector<string> colnames);//为一到多列添加主键
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
@@ -32,7 +79,7 @@ int main(int argc, char *argv[])
     string username = "";
     string password = "";
 
-    switch(regUser("zzz00","xyzzzzzz"))//测试用
+    switch(regUser("zzz00","xyzzzzzz"))//TODO:测试用
     {
     case 0:
         cout<<"注册成功！"<<endl;
@@ -60,6 +107,7 @@ int main(int argc, char *argv[])
         if(JudgeUser(username,password))
         {
             cout<<"登录成功！"<<endl;
+            string curDBname = "";
 			while(1)
 			{
 				int sqlType;//sql命令的类型，包括创建数据库、创建表、添加字段等正确操作，之后可扩展添加语法错误检查
@@ -67,11 +115,11 @@ int main(int argc, char *argv[])
 				vector<string> sqlkey;
 				
 				getline(cin,sql);//TODO:暂时没考虑换行的问题，即一句SQL语句是在一行中写完的，以分号结尾
-				sqlType = sqlAnalysis(sql,sqlkey);
+                sqlType = sqlAnalysis(sql,sqlkey);
 				switch()
 				{
 				case 0://创建数据库
-					switch(createDBEntity())
+                    switch(createDBEntity(sqlkey.at((1))))//TODO:
 					{
 					case 0:
 						cout<<"创建数据库成功！"<<endl;
@@ -81,7 +129,7 @@ int main(int argc, char *argv[])
 					}
 					break;
 				case 1://重命名数据库，语法规则：RENAME database olddbname TO newdbname
-					switch(renameDBEntity())
+                    switch(renameDBEntity())//TODO:
 					{
 					case 0:
 						cout<<"重命名数据库成功！"<<endl;
@@ -91,7 +139,7 @@ int main(int argc, char *argv[])
 					}
 					break;
 				case 2:
-					switch(dropDBEntity())
+                    switch(dropDBEntity(sqlkey.at((1))))//TODO:
 					{
 					case 0:
 						cout<<"删除数据库成功！"<<endl;
@@ -101,9 +149,10 @@ int main(int argc, char *argv[])
 					}
 					break;
 				case 3:
-					switch(useDBEntity())
+                    switch(useDBEntity(sqlkey.at((1))))//TODO:
 					{
 					case 0:
+                        curDBname = useDBEntity(sqlkey.at((1)));
 						cout<<"更换数据库成功！"<<endl;
 						break;
 					default:
@@ -114,9 +163,6 @@ int main(int argc, char *argv[])
 					
 					break;
 				case DT:
-					
-					break;
-				case RT:
 					
 					break;
 				default:
@@ -338,4 +384,49 @@ int initDBEntity(string DBname)//1代表初始化失败，0代表成功
 	if(dropDBEntity(DBname)!=0) return 1;
 	if(createDBEntity(DBname)!=0) return 1;
 	return 0;
+}
+
+int backupDBEntity(string DBname)//备份数据库
+{
+
+}
+
+int createTable(string TBname,string DBname,vector<Field> fields)//创建表
+{
+
+}
+
+int dropTable(string TBname,string DBname)//删除表
+{
+
+}
+
+int deleteField(string TBname,string DBname,string colname)//删除表中一列
+{
+
+}
+
+int addField(string TBname,string DBname,Field col)//增加一列
+{
+
+}
+
+int modifyCol(string TBname,string DBname,string oldname,string newname,string type,int length)//修改一列的名称及类型
+{
+
+}
+
+int renameTable(string TBname,string DBname,string newname)//重命名表
+{
+
+}
+
+int removePri(string TBname,string DBname)//删除主键
+{
+
+}
+
+int addPri(string TBname,string DBname,string priname,vector<string> colnames)//为一到多列添加主键
+{
+
 }
